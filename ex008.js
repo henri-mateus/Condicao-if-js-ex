83fd6b053d0f5498a627601b05e83a0c4596a13d
Mateus Henri <aqwmateus@outlook.com>
2024-07-01 17:26:44 -0300

    ex010.html

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 5;
+console.log(` A velocidade do seu carro é ${vel} km/h`);
+if (vel > 60) {
+  console.log(` A cima da velocidade `);
+}
+console.log(` Dirija sempre com cinto de segurança`);
